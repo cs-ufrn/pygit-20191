9a8735cec7d90bfdabf2bca8fbc5e35111192d5a
Pedro Fonsêca <pedrohsfonsecas@gmail.com>
2019-03-21 16:28:01 -0300

    Implementa as funções take, map e filter

--- source/funcional.py
+++ source/funcional.py
@@ -1,11 +1,28 @@
 """ Retorna uma lista contendo os ks primeiros elementos da lista """
 def take(lista, k):
-	pass
+	listaks = []
+
+	if k <= len(lista):
+		listaks = lista[:k]
+	else:
+		listaks = lista[:]
+	return listaks
 
 """ Retorna uma lista contendo o resultado da aplicação da função 'f' em cada elemento da lista """
 def map(lista, f):
-	pass
+	listaf = []
+	
+	for index in lista:
+		listaf.append(f(index))
+
+	return listaf
 
 """ Retorna os elementos da lista que satisfaçam a função f """
 def filter(lista, f):
-	pass
+	listaS = []
+	
+	for index in lista:
+		if f(index) == True:
+			listaS.append(index)
+
+	return listaS
