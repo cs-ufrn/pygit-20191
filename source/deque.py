3f39e9a134a01e6f77eb0840847e237c3e059838
olá <oi>
2019-03-21 17:33:05 -0300

    Implementar funções do deque

--- source/deque.py
+++ source/deque.py
@@ -1,23 +1,26 @@
+
 """ Insere um elemento no início do deque """
 def push_front(deque, elemento):
-	pass
-
+	deque.insert(0, elemento)
+	return deque
 """ Insere um elemento ao final do deque """
 def push_back(deque, elemento):
-	pass
+	deque.append(elemento)
+	return deque
 
 """ Retorna o elemento do início do deque """
 def front(deque):
-	pass
+	return deque[0]
 
 """ Retorna o elemento do final do deque """
 def back(deque):
-	pass
+	return deque[-1]
 
 """ Remove um elemento do início do deque """
 def pop_front(deque):
-	pass
-
+	del deque[0]
+	return deque
 """ Remove um elemento do final do deque """
 def pop_back(deque):
-	pass
+	deque.pop()
+	return deque
