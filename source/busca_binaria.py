e344e635cb7276949dae6f43ed2b7e5da84dd363
Daniel <danielmleiros@hotmail.com>
2019-03-21 17:11:40 -0300

    Faz Buscabinária

--- source/busca_binaria.py
+++ source/busca_binaria.py
@@ -1,3 +1,19 @@
 """ Retorna a posição do elemento na lista """
 def busca_binaria(lista, elemento):
-	pass
+	try:
+		lista_v = list(lista)
+	except ValueError:
+		print("The arg lista can not be parse to a type list")
+	
+	l=0
+	r=len(lista_v)-1
+
+	while(r >= l):	
+		middle = r+l//2
+		if(lista_v[middle] == elemento):
+			return middle
+		elif(lista_v[middle] < elemento):
+			l = middle +1
+		else:
+			r = middle - 1
+	return -1
