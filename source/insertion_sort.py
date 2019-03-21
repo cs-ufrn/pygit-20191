bb7e26c8db9574a7d034a718cea681e10c82b65b
jribeiro <jfrsnr@gmail.com>
2019-03-21 17:04:34 -0300

    Implement insertion sort

--- source/insertion_sort.py
+++ source/insertion_sort.py
@@ -1,3 +1,10 @@
 """ Retorna uma lista ordenada, utilizando o algoritmo insertion sort """
 def insertion_sort(lista):
-	pass
+	for j in range(1,len(lista)):
+		elemento = lista[j]
+		i = j-1
+		while (i>=0 and lista[i] > elemento):
+			lista[i+1] = lista[i]
+			i = i-1
+		lista[i+1] = elemento
+	return lista
