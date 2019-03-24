b60931604da149eedc0efd2e8860f8cbcdea51d0
root <root@DESKTOP-2UJLT1B.localdomain>
2019-03-24 14:38:39 -0300

    Mudando as funções para o tipos List

--- source/conjunto.py
+++ source/conjunto.py
@@ -1,13 +1,14 @@
 """ Insere um elemento no conjunto, caso ele não exista """
 def insert(conjunto, elemento):
-	conjunto.add(elemento)
+	conjunto.append(elemento)
         return conjunto
 
 """ Remove um elemento no conjunto, caso ele exista """
 def remove(conjunto, elemento):
-	conjunto.discard(elemento)
+	conjunto.remove(elemento)
         return conjunto
 
 """ Verifica se um elemento existe no conjunto """
 def exists(conjunto, elemento):
 	return elemento in conjunto
+
