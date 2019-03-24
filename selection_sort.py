6b7c2e7fdecb4bb120b63e3ff40c09a321fdd8b1
Lufec <lu_fe_c@hotmail.com>
2019-03-24 15:41:24 -0300

    mude local

--- selection_sort.py
+++
@@ -1,8 +0,0 @@
-def selection_sort(alist):		
-   for preencherLocal in range(len(alist)-1,0,-1):   #for que inicia do último elemento da lista e volta até o primeiro
-       posicaoDeMaximo=0				  #Max =0 é o indice do primeiro elemento, considerado máximo
-       for local in range(1,preencherLocal+1):	  # for que vai do segundo elemento até o elemento seguinte de fillslot
-           if alist[local]>alist[posicaoDeMaximo]:  #condicional para dizer qual elemento é o maior
-               posicaoDeMaximo = local		     #caso atendido, o índice do máximo é atualizado
-       alist[posicaoDeMaximo],alist[preencherLocal] = alist[preencherLocal], alist[posicaoDeMaximo]	#após encontrado, posições são trocadas: elemento máximo com a maior posição determinada pelo fillslot (será a maior posição não ordenada)
-
