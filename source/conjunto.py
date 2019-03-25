a56eea5b273abeb682ad508d1c9aad08860c40ea
root <root@DESKTOP-2UJLT1B.localdomain>
2019-03-25 20:57:03 -0300

    verificando a existencia do elemento no conjunto nas funções remove e insert

--- source/conjunto.py
+++ source/conjunto.py
@@ -1,14 +1,19 @@
+""" Verifica se um elemento existe no conjunto """
+def exists(conjunto, elemento):
+  return elemento in conjunto
+
 """ Insere um elemento no conjunto, caso ele não exista """
 def insert(conjunto, elemento):
-	conjunto.append(elemento)
-        return conjunto
+  if(exists(conjunto, elemento) == False):
+    conjunto.append(elemento)
+    return conjunto
+  else:
+    return conjunto
 
 """ Remove um elemento no conjunto, caso ele exista """
 def remove(conjunto, elemento):
-	conjunto.remove(elemento)
-        return conjunto
-
-""" Verifica se um elemento existe no conjunto """
-def exists(conjunto, elemento):
-	return elemento in conjunto
-
+  if(exists(conjunto, elemento) == True):
+    conjunto.remove(elemento)
+    return conjunto
+  else:
+    return conjunto
